81f33de51e0ebbde76f8de7315ba3789785fa560
helverton <helvertonsantos@gmail.com>
2017-01-06 23:32:14 -0200

    feature comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+
+  get '/comments' => 'posts#comments', as: 'comments'
+  post '/create_comments' => 'posts#create_comment', as: 'create_comment'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
